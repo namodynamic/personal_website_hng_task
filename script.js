9698ca665b377383eff21180b4b274d1b682405b
Nnamdi Cyprian Ekechi <nnamdi4u09@gmail.com>
2024-07-05 01:20:33 +0100

    Update UI

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
- // Get the current time in UTC
- const currentTimeUTC = new Date().toLocaleString("en-US", { timeZone: "UTC" });
- document.querySelector("[data-testid='currentTimeUTC']").textContent = currentTimeUTC;
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const currentDay = daysOfWeek[new Date().getDay()];
+document.querySelector("[data-testid='currentDay']").textContent = currentDay;
 
- // Get the current day of the week
- const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
- const currentDay = daysOfWeek[new Date().getDay()];
- document.querySelector("[data-testid='currentDay']").textContent = currentDay;
+function currentTimeUTC() {
+  const now = new Date();
+  const hours = now.getUTCHours().toString().padStart(2, "0");
+  const minutes = now.getUTCMinutes().toString().padStart(2, "0");
+  const seconds = now.getUTCSeconds().toString().padStart(2, "0");
+
+  const formattedTime = `${hours}:${minutes}:${seconds}`;
+
+  document.querySelector("[data-testid='currentTimeUTC']").textContent =
+    formattedTime;
+}
+
+currentTimeUTC();
+setInterval(currentTimeUTC, 1000);
